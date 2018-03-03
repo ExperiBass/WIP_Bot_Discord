c99a2a2199bc764025ceb1c1497a4c050e4d266a
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 18:14:15 -0500

    Created info command

---
+++ commands/info.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'info',
+    description: 'info about bot',
+    execute(message, args) {
+        message.channel.send(Created by GingkathFox#3335! **In Development**');
+    },
+};
