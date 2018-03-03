8046c0fd600ee2126ee022e7afde504e43aea417
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 18:45:35 -0500

    DYNAMIC COMMANDS!!! XD

--- TestBot.js
+++ TestBot.js
@@ -21,15 +21,15 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'ping') {
-        message.channel.send('Pong.');
-    }
-  else if (command === 'info') {
-        message.channel.send('Created by GingkathFox#3335! **In Devolopment**');
-    }
-  else if (command === 'givetoken') {
-        message.channel.send('Nope! *You failed!*');
-    }
+   if (!client.commands.has(command)) return;
+
+try {
+    client.commands.get(command).execute(message, args);
+}
+catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+}
 });
 
 client.login(token);
