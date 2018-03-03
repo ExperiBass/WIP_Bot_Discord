9280951f601cc0ab4df8909cc86d3a99e44f8ae9
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 18:34:39 -0500

    Update TestBot.js

--- TestBot.js
+++ TestBot.js
@@ -1,21 +1,35 @@
+const fs = require('fs');
 const Discord = require("discord.js");
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection()
+
+const commandFiles = fs.readdirSync('./commands');
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.on("ready", () => {
   console.log("I'M UP!!! sheesh...");
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith(`${prefix}ping`)) {
-    message.channel.send("pong!");
-  }
-  if (message.content.startsWith(`${prefix}info`)) {
-    message.channel.send("Created by GingkathFox#3335! **In Development**");
-  }
-  if (message.content.startsWith(`${prefix}givetoken`)) {
-    message.channel.send("Nope! *You failed!*");
-  }
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('Pong.');
+    }
+  else if (command === 'info') {
+        message.channel.send('Created by GingkathFox#3335! **In Devolopment**');
+    }
+  else if (command === 'givetoken') {
+        message.channel.send('Nope! *You failed!*');
+    }
 });
 
 client.login(token);
