1f316c2635e070227bd6c5381f75564f8256fa07
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 18:57:01 -0500

    Server info command!
    displays server name, users in the server (Including bots), and the date the server was created!

---
+++ commands/serverinfo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'serverinfo',
+    description: 'Info about the server!',
+    execute(message, args) {
+        message.channel.send('**SERVERINFO**\n Name: ${message.guild.name}\n Total members: ${message.guild.memberCount}, includng bots!\n Date created on: ${message.guild.createdAt}');
+    },
+};
