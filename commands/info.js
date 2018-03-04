151a5d9aa5fb721f180064d058802a6c2e0f10a5
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 23:37:42 -0500

    Syntax error

--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'info',
     description: 'info about bot',
     execute(message, args) {
-        message.channel.send(Created by GingkathFox#3335! **In Development**');
+        message.channel.send("Created by GingkathFox#3335! **In Development**");
     },
 };
