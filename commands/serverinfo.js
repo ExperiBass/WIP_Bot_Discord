bb0be8ef160b8597c88ae1ecd6126906540cc348
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-04 02:22:23 -0500

    PlzBeFixed

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Info about the server!',
     execute(message, args) {
         
-message.channel.send(`${user}\n**SERVERINFO**\n Name: ${message.guild.name}\n Total members: ${message.guild.memberCount}, includng bots!\n Date created on: ${message.guild.createdAt}`);
+message.channel.send(`${user}\n**SERVERINFO**\nName: ${message.guild.name}\nTotal members: ${message.guild.memberCount}, includng bots!\nDate created on: ${message.guild.createdAt}`);
     },
 };
