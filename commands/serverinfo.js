7bff100d739ccf7dd7c519ad875f4f934794fbf4
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-03 23:48:30 -0500

    Syntax error

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,6 +2,8 @@ module.exports = {
     name: 'serverinfo',
     description: 'Info about the server!',
     execute(message, args) {
-        message.channel.send('${user}\n**SERVERINFO**\n Name: ${message.guild.name}\n Total members: ${message.guild.memberCount}, includng bots!\n Date created on: ${message.guild.createdAt}');
+        ```js
+message.channel.send(`${user}\n**SERVERINFO**\n Name: ${message.guild.name}\n Total members: ${message.guild.memberCount}, includng bots!\n Date created on: ${message.guild.createdAt}`);
+```
     },
 };
