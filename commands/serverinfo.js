b806ba05bd67a5c31b675c0fb8e429a6a152c150
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-04 02:08:48 -0500

    Fixed error (hopefully)

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'serverinfo',
     description: 'Info about the server!',
     execute(message, args) {
-        ```js
+        
 message.channel.send(`${user}\n**SERVERINFO**\n Name: ${message.guild.name}\n Total members: ${message.guild.memberCount}, includng bots!\n Date created on: ${message.guild.createdAt}`);
-```
     },
 };
