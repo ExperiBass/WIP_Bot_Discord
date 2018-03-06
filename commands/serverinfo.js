5e232b261b5b30333186b88f62605f5b969eb74e
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-06 17:23:55 -0500

    Edited bug

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'serverinfo',
     description: 'Info about the server!',
     execute(message, args) {
-    var Humans = `${message.guild.memberCount` - 4
-message.channel.send(`**SERVER INFO:**\nName: ${message.guild.name}\nTotal members: " + Humans + ", includng bots!\nDate created on: ${message.guild.createdAt}`);
+    const Humans = `${message.guild.memberCount` - 4
+message.channel.send(`**SERVER INFO:**\nName: ${message.guild.name}\nTotal members: ` + Humans + `, includng bots!\nDate created on: ${message.guild.createdAt}`);
     },
 };
