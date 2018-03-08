992effc692b892a2e94415a54bccb5eea7db00f6
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-08 18:53:19 -0500

    Added command and reply

--- commands/github.js
+++ commands/github.js
@@ -1,1 +1,7 @@
-https://goo.gl/Hu1gF1
+module.exports = {
+    name: 'github',
+    description: 'Want to help develop?',
+    execute(message, args) {
+        message.channel.send('Wabt to help develop? \nClick this link! https://goo.gl/Hu1gF1');
+    },
+};
