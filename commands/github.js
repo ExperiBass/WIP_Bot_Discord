edc20391b25ac254afff6421b5b3901c46dae3ad
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-08 18:53:50 -0500

    Killed a typo

--- commands/github.js
+++ commands/github.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'github',
     description: 'Want to help develop?',
     execute(message, args) {
-        message.channel.send('Wabt to help develop? \nClick this link! https://goo.gl/Hu1gF1');
+        message.channel.send('Want to help develop? \nClick this link! https://goo.gl/Hu1gF1');
     },
 };
