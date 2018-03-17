9acf7c0a8a51b5b0bcd8f0cf1e316cfa56bd1c7e
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-17 15:16:18 -0400

    consoleLog!

--- commands/github.js
+++ commands/github.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'github',
     description: 'Want to help develop?',
     execute(message, args) {
+        console.log("oooo, someone might want to help!");
         message.channel.send('Want to help develop? \nClick this link! https://goo.gl/Hu1gF1');
     },
 };
