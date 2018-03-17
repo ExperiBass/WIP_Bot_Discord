b407af2c269d933fb2d91637ccce6eb08d66618b
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-17 15:19:50 -0400

    witty console comments XD

--- commands/givetoken.js
+++ commands/givetoken.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'givetoken',
     description: 'Prank your friends!',
     execute(message, args) {
+        console.log("Someone got pranked!");
         message.channel.send('Nope! *You failed!*');
     },
 };
