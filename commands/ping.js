2db4e2099c905b2ea13748b8bdfd8bc7fb4369a1
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-03-17 15:18:13 -0400

    pingapingaping

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     execute(message, args) {
+       console.log("Ping was used!");
         message.channel.send('Pong!');
     },
 };
