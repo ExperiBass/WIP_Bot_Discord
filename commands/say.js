324a575244db1ca4886498ac7e38815ea9831e6b
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-15 18:41:56 -0400

    Commented out command restriction

--- commands/say.js
+++ commands/say.js
@@ -2,10 +2,11 @@ module.exports = {
        name: `say`,
        description: `The bot will delete your message, then repeat what you said.`,
        execute(message, args) { 
-   if (message.author.id !== 'YOUR_USER-ID_HERE') {
+   /*if (message.author.id !== 'YOUR_USER-ID_HERE') {
           channel.message.send("You don\'t have permission to use this command!");
           return;
-   }
+   }*/
+             
    const sayMessage = args.join(" ");
     
     message.delete().catch(O_o=>{}); 
