55e609f75a75ccd303bc8084d4198c622e0d3c4b
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-15 00:59:44 -0400

    Restricting use :>

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,11 @@ module.exports = {
        name: `say`,
        description: `The bot will delete your message, then repeat what you said.`,
        execute(message, args) { 
-    const sayMessage = args.join(" ");
+   if (message.author.id !== 'YOUR_USER-ID_HERE') {
+          channel.message.send("You don\'t have permission to use this command!");
+          return;
+   }
+   const sayMessage = args.join(" ");
     
     message.delete().catch(O_o=>{}); 
     
