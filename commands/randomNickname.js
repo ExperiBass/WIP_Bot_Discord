934d9c3bd07c21506cb698b32e86c9c4408c40a6
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-27 12:16:03 -0400

    Fixed `randomNickname.js`

--- commands/randomNickname.js
+++ commands/randomNickname.js
@@ -1,8 +1,8 @@
 module.exports = {
-       name: `nickname`, //what the user would type to activate the command, like "ping"
-       description: `changes your nickname to one of 53 possible names!`, //description of the command, used in the help DM
+       name: `nickname`,
+       description: `changes your nickname to one of 53 possible names!`,
        execute(message, args) {
              randNick()
-             message.channel.send(`Your nickname has been changed to ` + randNick + `!`) //what you want the bot to say when the user uses the command
+             message.channel.send(`Your nickname has been changed to ` + randNick + `!`)
       },
 };
