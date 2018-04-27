77a3e2ec27e423ce8135c6e049bff0bda328683c
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-26 23:07:40 -0400

    Create randNick.js

---
+++ commands/randNick.js
@@ -0,0 +1,169 @@
+function randNick() {
+// defining variables
+var Nickname;
+var randNum = Math.floor(Math.random)
+// end defining variables, begin fallback
+if (Nickname.length > 30) {
+Nickname = 'Over 30!'}
+// Eld fallback, begin long if-else chain
+if (randNum == 0.01){
+    Nickname = 'REEEE!'}
+else if (randNum == 0.02)  {
+    Nickname = 'Benjamin Dover'
+}
+else if (randNum == 0.03)  {
+    Nickname = 'BEGONE, THOT!' 
+}
+else if (randNum == 0.04)  {
+    Nickname = 'OwO wats dis?'    
+}
+else if (randNum == 0.05)  {
+    Nickname = 'Welp. Yer Dead.'   
+}
+else if (randInt == 0.06)  {
+    Nickname = 'Halp!'                     
+}
+else if (randNum == 0.07)   {
+    Nickname = 'i like twains.'
+}
+else if (randNum == 0.08)   {
+    Nickname = 'Yokoka!'                
+}
+else if (randNum == 0.09)   {
+    Nickname = 'I’m Batman!'          
+}
+else if (randNum == 0.10)    {
+    Nickname = 'I Am Groot!'            
+}
+else if (randNum == 0.11)     {
+    Nickname = 'Showoff!'                
+}
+else if (randNum == 0.12)     {
+    Nickname = 'Nico Nico Nii~!'      
+}
+else if (randNum == 0.13)     {
+    Nickname = 'BURN IT!!!'              
+}
+else if (randNum == 0.14)     {
+    Nickname = 'Torch It.'                   
+}
+else if (randNum == 0.15)     {
+    Nickname = 'U w0t m8?'             
+}
+else if (randNum == 0.16)     {
+    Nickname = 'OH! MY! GOSH!'      
+}
+else if (randNum == 0.17)      {
+    Nickname = 'Stevie Wonder'        
+}
+else if (randNum == 0.18)      {
+    Nickname = 'Zyn! ZynZynZyn!'    
+}
+else if (randNum == 0.19)      {
+    Nickname = 'Its only $300!'        
+}
+else if (randNum == 0.20)      {
+    Nickname = 'I’m with stupid ->'   
+}
+else if (randNum == 0.21)       {
+    Nickname = '<- Is Slep'               
+}
+else if (randNum == 0.22)       {
+    Nickname = '-3-'                          
+}
+else if (randNum == 0.23)       {
+    Nickname = 'o3o'                           
+}
+else if (randNum == 0.24)       {
+    Nickname = 'Made With Love!'    
+}
+else if (randNum == 0.25)       {
+    Nickname = '( ͡° ͜ʖ ͡°)'                      
+}
+else if (randNum == 0.26)       {
+    Nickname = '...Should we kill it?'  
+}
+else if (randNum == 0.27) {
+    Nickname = 'xXRobloXPlayerXx'
+}
+else if (randNum == 0.28) {
+    Nickname = 'Perceptional_Hamster'
+}
+else if (randNum == 0.29) {
+    Nickname == 'IRandomlyPickedThis'
+}
+else if (randNum == 0.30) {
+    Nickname = 'Ping Me!'
+}
+else if (randNum == 0.31) {
+    Nickname = 'TotallyLegit'
+}
+else if (randNum == 0.32) {
+    Nickname = 'OwO'
+}
+else if (randNum == 0.33) {
+    Nickname = 'BWAH BWAH BWAH BWAH BWAH BWAH!'
+}
+else if (randNum == 0.34) {
+    Nickname = 'F!te me m8'
+}
+else if (randNum == 0.35) {
+    Nickname = 'Bweh.'
+}
+else if (randNum == 0.36) {
+    Nickname = 'i like boulder.'
+}
+else if (randNum == 0.37) {
+    Nickname = ':b'
+}
+else if (randNum == 0.38) {
+    Nickname = 'FULL POWER!'
+}
+else if (randNum == 0.39) {
+    Nickname = '*nerd joke here*'
+}
+else if (randNum == 0.40) {
+    Nickname = 'Number: 40'
+}
+else if (randNum == 0.41) {
+    Nickname = 'Obrohom Link'
+}
+else if (randNum == 0.42) {
+    Nickname = 'smertz'
+}
+else if (randNum == 0.43) {
+    Nickname = 'C3-P0'
+}
+else if (randNum == 0.44) {
+    Nickname = 'Cr1ng3 Ma$t3r'
+}
+else if (randNum == 0.45) {
+    Nickname = 'CODE RED'
+}
+else if (randNum == 0.46) {
+    Nickname = 'HYPER LUL'
+}
+else if (randNum == 0.47) {
+    Nickname = 'Hello, World!'
+}
+else if (randNum == 0.48) {
+    Nickname = 'Supreme Burger!'
+}
+else if (randNum == 0.49) {
+    Nickname = 'SEEEEGAAAA!'
+}
+else if (randNum == 0.50) {
+    Nickname = 'Snoipins a good job m8'
+}
+else if (randNum == 0.51) {
+    Nickname = 'I wanna was the'
+}
+else if (randNum == 0.52) {
+    Nickname = 'Yfa'
+}
+else if (randNum == 0.53) {
+    Nickname = 'Fren, Foite Meh'
+}
+// 53, testing now
+return Nickname;
+}
