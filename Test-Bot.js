3b3ecf9e3e4dd39f642597da946b62cc02f11833
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-05-14 03:10:58 -0400

    Created new command `pstatus.js`, updates

--- Test-Bot.js
+++ Test-Bot.js
@@ -16,12 +16,36 @@ client.on("ready", () => {
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+return;
+}
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));
+
+function parseArgs (argString, argCount, allowSingleQuote = true) {
+  // Replace smart quotes (i.e. “ ” ‘ ’ ) with straight double quotes " "
+  argString = argString.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"')
+  const re = allowSingleQuote ? /\s*(?:("|')([^]*?)\1|(\S+))\s*/g : /\s*(?:(")([^]*?)"|(\S+))\s*/g
+  const result = []
+  let match = []
+  // Large enough to get all items
+  argCount = argCount || argString.length
+  // Get match and push the capture group that is not null to the result
+  while (--argCount && (match = re.exec(argString))) result.push(match[2] || match[3])
+  // If text remains, push it to the array as-is (except for wrapping quotes, which are removed)
+  if (match && re.lastIndex < argString.length) {
+    const re2 = allowSingleQuote ? /^("|')([^]*)\1$/g : /^(")([^]*)"$/g
+    result.push(argString.substr(re.lastIndex).replace(re2, '$2'))
+  }
+  return result
+}
+
+    const args = parseArgs(message.content.slice(prefix.length));
     const command = args.shift().toLowerCase();
 
-   if (!client.commands.has(command)) return;
+   if (!client.commands.has(command)) {
+return;
+}
 
 try {
     client.commands.get(command).execute(message, args);
---
+++ randNick.js
@@ -0,0 +1,310 @@
+module.exports = randNick
+function randNick(Nickname) {
+// defining variables
+var Nickname;
+var randNum = Math.floor(Math.random)
+// end defining variables, begin long if-else chain
+if (randNum == 0.01){
+    Nickname = 'REEEE!'}
+else if (randNum == 0.02)  {
+    Nickname = 'Benjamin Dover'
+}
+else if (randNum == 0.03)  {
+    Nickname = 'BEGONE, THOT!' 
+}
+else if (randNum == 0.04)  {
+    Nickname = 'OwO wats dis?'    
+}
+else if (randNum == 0.05)  {
+    Nickname = 'Welp. Yer Dead.'   
+}
+else if (randNum == 0.06)  {
+    Nickname = 'Halp!'                     
+}
+else if (randNum == 0.07)   {
+    Nickname = 'i like twains.'
+}
+else if (randNum == 0.08)   {
+    Nickname = 'Yokoka!'                
+}
+else if (randNum == 0.09)   {
+    Nickname = 'I\’m Batman!'          
+}
+else if (randNum == 0.10)    {
+    Nickname = 'I Am Groot!'            
+}
+else if (randNum == 0.11)     {
+    Nickname = 'Showoff!'                
+}
+else if (randNum == 0.12)     {
+    Nickname = 'Nico Nico Nii~!'      
+}
+else if (randNum == 0.13)     {
+    Nickname = 'BURN IT!!!'              
+}
+else if (randNum == 0.14)     {
+    Nickname = 'Torch It.'                   
+}
+else if (randNum == 0.15)     {
+    Nickname = 'U w0t m8?'             
+}
+else if (randNum == 0.16)     {
+    Nickname = 'OH! MY! GOSH!'      
+}
+else if (randNum == 0.17)      {
+    Nickname = 'Stevie Wonder'        
+}
+else if (randNum == 0.18)      {
+    Nickname = 'Zyn! ZynZynZyn!'    
+}
+else if (randNum == 0.19)      {
+    Nickname = 'Its only $300!'        
+}
+else if (randNum == 0.20)      {
+    Nickname = 'I\’m with stupid ->'   
+}
+else if (randNum == 0.21)       {
+    Nickname = '<- Is Slep'               
+}
+else if (randNum == 0.22)       {
+    Nickname = '-3-'                          
+}
+else if (randNum == 0.23)       {
+    Nickname = 'o3o'                           
+}
+else if (randNum == 0.24)       {
+    Nickname = 'Made With Love!'    
+}
+else if (randNum == 0.25)       {
+    Nickname = '( Í¡Â° ÍœÊ– Í¡Â°)'                      
+}
+else if (randNum == 0.26)       {
+    Nickname = '...Should we kill it?'  
+}
+else if (randNum == 0.27) {
+    Nickname = 'xXRobloXPlayerXx'
+}
+else if (randNum == 0.28) {
+    Nickname = 'Perceptional_Hamster'
+}
+else if (randNum == 0.29) {
+    Nickname == 'IRandomlyPickedThis'
+}
+else if (randNum == 0.30) {
+    Nickname = 'Ping Me!'
+}
+else if (randNum == 0.31) {
+    Nickname = 'TotallyLegit'
+}
+else if (randNum == 0.32) {
+    Nickname = 'OwO'
+}
+else if (randNum == 0.33) {
+    Nickname = 'BWAH BWAH BWAH BWAH BWAH BWAH!'
+}
+else if (randNum == 0.34) {
+    Nickname = 'F!te me m8'
+}
+else if (randNum == 0.35) {
+    Nickname = 'Bweh.'
+}
+else if (randNum == 0.36) {
+    Nickname = 'i like boulder.'
+}
+else if (randNum == 0.37) {
+    Nickname = ':b'
+}
+else if (randNum == 0.38) {
+    Nickname = 'FULL POWER!'
+}
+else if (randNum == 0.39) {
+    Nickname = '*nerd joke here*'
+}
+else if (randNum == 0.40) {
+    Nickname = 'Number: 40'
+}
+else if (randNum == 0.41) {
+    Nickname = 'Obrohom Link'
+}
+else if (randNum == 0.42) {
+    Nickname = 'smertz'
+}
+else if (randNum == 0.43) {
+    Nickname = 'C3-P0'
+}
+else if (randNum == 0.44) {
+    Nickname = 'Cr1ng3 Ma$t3r'
+}
+else if (randNum == 0.45) {
+    Nickname = 'CODE RED'
+}
+else if (randNum == 0.46) {
+    Nickname = 'HYPER LUL'
+}
+else if (randNum == 0.47) {
+    Nickname = 'Hello, World!'
+}
+else if (randNum == 0.48) {
+    Nickname = 'Supreme Burger!'
+}
+else if (randNum == 0.49) {
+    Nickname = 'SEEEEGAAAA!'
+}
+else if (randNum == 0.50) {
+    Nickname = 'Snoipins a good job m8'
+}
+else if (randNum == 0.51) {
+    Nickname = 'I wanna was the'
+}
+else if (randNum == 0.52) {
+    Nickname = 'Yfa'
+}
+else if (randNum == 0.53) {
+    Nickname = 'Fren, Foite Meh'
+}
+else if (randNum == 0.54) {
+    Nickname = 'REEE!'
+}
+else if (randNum == 0.55) {
+    Nickname = '$' + randNum + 'k!'
+}
+else if (randNum == 0.56) {
+    Nickname = 'LMMS'
+}
+else if (randNum == 0.57) {
+    Nickname = 'stop.'
+}
+else if (randNum == 0.58) {
+    Nickname = 'prefix?'
+}
+else if (randNum == 0.59) {
+    Nickname = 'xXpythonXx'
+}
+else if (randNick == 0.60) {
+    Nickname = 'Dart Gun!!!'
+}
+else if (randNum == 0.61) {
+    Nickname = '._._._._.'
+}else if (randNum == 0.62) {
+    Nickname = '.-.-.-.-.'
+}
+else if (randNum == 0.63) {
+    Nickname = 'Seiko!'
+}
+else if (randNum == 0.64) {
+    Nickname = 'I have ' + randNum + 'Moniez!'
+}
+else if (randNum == 0.65) {
+    Nickname = 'Catan!'
+}
+else if (randNum == 0.66) {
+    Nickname = 'ioB'
+}
+else if (randNum == 0.67) {
+    Nickname = 'IRIS is Siri'
+}
+else if (randNum == 0.68) {
+    Nickname = msg.member.nickname + '68'
+}
+else if (randNum == 0.69) {
+    Nickname = 'this is torture.'
+}
+else if (randNum == 0.70) {
+    Nickname = 'VOLTRON!'
+}
+else if (randNum == 0.71) {
+    Nickname = 'im dead'
+}
+else if (randNum == 0.72) {
+    Nickname = 'indent by 4 spaces.'
+}
+else if (randNum == 0.73) {
+    Nickname = 'ree. ree? ree! ree?!?'
+}
+else if (randNum == 0.74) {
+    Nickname = 'return Member;'
+}
+else if (randNum == 0.75) {
+    Nickname = 'Nickname = \' Nickname ='
+}
+else if (randNum == 0.76) {
+    Nickname = 'else if?'
+}
+else if (randNum == 0.77) {
+    Nickname = 'Test-Bot'
+}
+else if (randNum == 0.78) {
+    Nickname = 'Cancer.'
+}
+else if (randNum == 0.79) {
+    Nickname = 'K-Pop is awesome!'
+}
+else if (randNum == 0.80) {
+    Nickname = 'BOB THE BUILDER'
+}
+else if (randNum == 0.81) {
+    Nickname = '\[T]/'
+}
+else if (randNum == 0.82) {
+    Nickname = '18.0'
+}
+else if (randNum == 0.83) {
+    Nickname = 'Areee!'
+}
+else if (randNum == 0.84) {
+    Nickname = 'Ivy!'
+}
+else if (randNum == 0.85) {
+    Nickname = '{}[]<>()'
+}
+else if (randNum == 0.86) {
+    Nickname = 'Made with VSCode!'
+}
+else if (randNum == 0.87) {
+    Nickname = '4TH WALL!!!'
+}
+else if (randNum == 0.88) {
+    Nickname = 'Cailou'
+}
+else if (randNum == 0.89) {
+    Nickname = '^ has 3 copyright strikes'
+}
+else if (randNum == 0.90) {
+    Nickname = 'JJJ'
+}
+else if (randNum == 0.91) {
+    Nickname = 'GG SCRUBZ'
+}
+else if (randNum == 0.92) {
+    Nickname = '010101110'
+}
+else if (randNum == 0.93) {
+    Nickname = 'randNum'
+}
+else if (randNum == 0.94) {
+    Nickname = msg.member.nickname + '()'
+}
+else if (randNum == 0.95) {
+    Nickname = 'whee!'
+}
+else if (randNum == 0.96) {
+    Nickname = 'Queen'
+}
+else if (randNum == 0.97) {
+    Nickname = 'Knight name'
+}
+else if (randNum == 0.98) {
+    Nickname = 'Ì”ÌÌŠÌšÌÍ§ÍŠÌ§ÍžÍÌ·Ì•ÌŸÌ­ÌºÍ•ÌœÌ¦'
+}
+else if (randNum == 0.99) {
+    Nickname = 'ABCD...WXYZ'
+}
+// end if else, begin fallback
+if (Nickname.length > 32) {
+    Nickname = 'Over 32!'
+    message.channel.send('Uh oh, the nickname is over 30 characters! /nPlease tell `GingkathFox#3335` about this and send this number:' + randNum)
+}
+//end fallback
+msg.member.setNickname(Nickname, 'Furry Fam joke :3')
+return Nickname;
+}
