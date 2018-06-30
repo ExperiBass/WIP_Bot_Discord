0819cd59db8e789cf67f3c6bd002d62d913c303e
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-06-30 16:59:04 -0400

    `randomNickname.js` Fixed nav(maybe)

--- commands/randomNickname.js
+++ commands/randomNickname.js
@@ -2,7 +2,7 @@ module.exports = {
       name: `nickname`,
       description: `Changes your nickname to one of 99 possible names!`,
       execute(message, args) {
-            var randNick = require('./randNick.js')
+            var randNick = require('./commands/functions/randNick.js')
             let newNick = randNick(message)
             if (newNick) message.channel.send(`Your nickname has been changed to ` + newNick + `!`)
      },
