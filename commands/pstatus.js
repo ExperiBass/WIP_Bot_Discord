a6173333845b2fb29176b649a2150373b563f83a
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-07-13 10:15:52 -0400

    `pstatus.js` added return statements

--- commands/pstatus.js
+++ commands/pstatus.js
@@ -6,7 +6,8 @@ module.exports = {
         
         if (!args.length) {
         message.channel.reply('You didn\'t give me a new game! ;w;');
-            }
+        return;    
+        }
 
         let newGame = args[0, 1, 2, 3]
         let activityType= args[4]
@@ -14,12 +15,14 @@ module.exports = {
 
         if (!activityTypes.includes(args[4]) ) {
             message.channel.send('You didn’t send an activity type! The types are: ' + activityTypes);
-
-            client.user.setActivity(newGame);
-            message.channel.send('Playing status has been set to "' + newGame + '"!');
-
-            console.log('Playing status was changed to ' + newGame);
+            return;
         }
+
+        client.user.setActivity(newGame);
+        message.channel.send('Playing status has been set to "' + newGame + '"!');
+
+        console.log('Playing status was changed to ' + newGame);
+            
         if (args[0] == 'help') {
             message.channel.reply('**HOW TO USE:** /nThe first four words after the command will be the game! The fifth will be what I am doing with it (Listening, Watching, Playing, Streaming)! /nPlease type the fifth word in ALL CAPS, otherwise it won’t work!');
         }
